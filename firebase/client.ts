ff2ab695953dc6f87788a242e9f9fd8a1126352e
Mainak Das <dasmainak2018@gmail.ccom>
2025-04-09 13:11:00 +0530

    envv changes

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,13 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAlieGPwLY25D6Hy8l5qJ8PThHc9UkMPHo",
   authDomain: "prepwise-ee272.firebaseapp.com",
